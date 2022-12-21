ce6bb8a35c5e510296f666e24787c2d823252bee
mostafiz9900 <mostafiz9900@gmail.com>
2022-12-21 23:17:54 +0600

    sds

---
+++ lib/src/todo/domain/entities/todo.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/foundation.dart' show immutable;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:uuid/uuid.dart';
+
+const _uuid = Uuid();
+
+/// A read-only description of a todo-item
+@immutable
+class Todo {
+  const Todo({
+    required this.description,
+    required this.id,
+    this.completed = false,
+  });
+
+  final String id;
+  final String description;
+  final bool completed;
+
+  @override
+  String toString() {
+    return 'Todo(description: $description, completed: $completed)';
+  }
+}
